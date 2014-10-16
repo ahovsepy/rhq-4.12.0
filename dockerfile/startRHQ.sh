cee7e4eaa32588574b35c24c10f07ef963d2cad0
Armine Hovsepyan <ahovsepy@redhat.com>
2014-10-16 14:07:39 +0200

    trap added into start script

--- dockerfile/startRHQ.sh
+++ dockerfile/startRHQ.sh
@@ -1,5 +1,17 @@
+trap 'my_exit; exit' SIGINT SIGQUIT
+count=0
+
+my_exit()
+{
+echo "you hit Ctrl-C/Ctrl-\, now exiting.."
+ su root -c '/opt/rhq-server-4.12.0/bin/rhqctl stop'
+}
+#start postgresql server
 su -l postgres -c " pg_ctl -l server.log -w stop; pg_ctl -l server.log -w start; "
 echo "Waiting 30s for DB to startup"
 sleep 30s
+#start rhq server/storage/agent
 su root -c '/opt/rhq-server-4.12.0/bin/rhqctl start'
+#tail server.log
+su root -c 'tail -f /opt/rhq-server-4.12.0/logs/server.log'
 
