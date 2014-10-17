22647ac88874938cd3f3abd4ac0a2d7cb0d1986f
ahovsepy <ahovsepy@dhcp130-204.brq.redhat.com>
2014-10-17 14:11:47 +0200

    start script tuned -- known issue for rhqctl install --start

--- dockerfile/startRHQ.sh
+++ dockerfile/startRHQ.sh
@@ -10,8 +10,9 @@ echo "you hit Ctrl-C/Ctrl-\, now exiting.."
 su -l postgres -c " pg_ctl -l server.log -w stop; pg_ctl -l server.log -w start; "
 echo "Waiting 30s for DB to startup"
 sleep 30s
-#start rhq server/storage/agent
-su root -c '/opt/rhq-server-4.12.0/bin/rhqctl install --start'
+#install and start rhq server/storage/agent
+su root -c '/opt/rhq-server-4.12.0/bin/rhqctl install'
+su root -c '/opt/rhq-server-4.12.0/bin/rhqctl start'
 #tail server.log
 su root -c 'tail -f /opt/rhq-server-4.12.0/logs/server.log'
 
